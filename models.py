83add6006bad9c1a1ccd5441ad123b3007fc0d22
utkarshamande <utkarshamande4@gmail.com>
2025-05-13 12:13:15 +0000

    Contains the database models for the Expense Tracker, including classes and methods for interacting with the database.

--- models.py
+++ models.py
@@ -8,3 +8,6 @@ class Expense(db.Model):
     amount = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(50), nullable=False)
     date = db.Column(db.Date, nullable=False)
+
+    def __repr__(self):
+        return f'<Expense {self.title}>'
