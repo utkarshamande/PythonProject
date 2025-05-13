ce1cda51b5e71cf51a7b928fa824843f3e0027bb
utkarshamande <utkarshamande4@gmail.com>
2025-05-13 09:38:49 +0000

    Contains the database models for the Expense Tracker, including classes and methods for interacting with the database.

---
+++ models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False)
